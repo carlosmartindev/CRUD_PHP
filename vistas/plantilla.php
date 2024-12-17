cf18c26463771cd6ab7b789a52936b6fe1a62a8e
Carlos Martin <carlosmartin.dev@gmail.com>
2024-12-17 02:35:44 -0600

    Primer commit. Se crea la estructura base para el proyecto

---
+++ vistas/plantilla.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Administrador de tareas</title>
+	<meta name="author" content="Carlos Martin" />
+	<meta name="description" content="Sistema desarrollado para la administración de tareas del usuario"/>
+	<meta http-equiv="cache-control" content="no-cache"/>
+
+	<!-- CSS -->
+	<link href="vistas/assets/css/styles.css" rel="stylesheet" />
+
+	<!-- JS -->
+	<script src="https://kit.fontawesome.com/83819efe74.js" crossorigin="anonymous"></script>
+	<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+</head>
+<body>
+	<h1 class="h1_principal">Administrador de tareas</h1>
+</body>
+</html>
